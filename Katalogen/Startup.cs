71758eacea145eec33377dbdfc3f4b4d960cd41e
Torbjörn Wejheden <torwej@gmail.com>
2015-06-11 14:38:00 +0200

    A basic solution

---
+++ Katalogen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Katalogen.Startup))]
+namespace Katalogen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
